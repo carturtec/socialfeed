6cab629e135da1cbf466caa3e846aa6e74075f29
karlos <carthurtec@gmail.com>
2023-05-29 03:01:38 -0300

    near of the end commit

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,13 +1,22 @@
 import { ThumbsUp, Trash } from 'phosphor-react';
 import styles from './Comment.module.css';
 import { Avatar } from './Avatar';
+import { useState } from 'react';
 
 // eslint-disable-next-line react/prop-types
 export function Comment({content, onDeleteComment}) {
+  const [likeCount, setLikeCount] = useState(0);
 
   function handleDeleteComment() {
     onDeleteComment(content)
   }
+
+  function handleLikeComment() {
+    setLikeCount((state) => {
+      return state + 1;
+    })
+  }
+
   return(
     <div className={styles.comment}>
       <Avatar hasBorder={false} src="./src/assets/neuber.png" />
@@ -29,9 +38,9 @@ export function Comment({content, onDeleteComment}) {
         </div>
 
         <footer>
-         <button>
+         <button onClick={handleLikeComment}>
           <ThumbsUp />
-          Aplaudir <span>20</span>
+          Aplaudir <span>{likeCount}</span>
          </button>
         </footer>
        </div>
